bca56b3c8f4d528bd7bb83daf7f7cea1472011b5
Jun Takahashi <jtakahas@princeton.edu>
2015-03-25 11:02:55 -0400

    redirect to Google Forms

--- main.py
+++ main.py
@@ -19,7 +19,11 @@ class FeedbackHandler(webapp2.RequestHandler):
 		template = JINJA_ENVIRONMENT.get_template('templates/feedback.html')
 
 		template_values = {'latlng': self.request.get('latlng') }
-		self.response.write(template.render(template_values))
+
+		url = 'https://docs.google.com/forms/d/1gEWl28gtcfvVr1-2D9e9yf556mKnvN2KBSUol6t1hnY/viewform' \
+			+ '?entry.439758060=' + str(self.request.get('latlng'))
+		self.redirect(url)
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
