4273464ff4cef4ef860507e0df7f95f1991944c3
Jun Takahashi <jtakahas@princeton.edu>
2015-03-25 11:42:38 -0400

    server side config json file created

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import urllib
 import jinja2
 import webapp2
+import json
+
+with open('server_config.json') as config_file:
+	config_json = json.load(config_file)
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
@@ -21,7 +25,7 @@ class FeedbackHandler(webapp2.RequestHandler):
 		template_values = {'latlng': self.request.get('latlng') }
 
 		url = 'https://docs.google.com/forms/d/1gEWl28gtcfvVr1-2D9e9yf556mKnvN2KBSUol6t1hnY/viewform' \
-			+ '?entry.439758060=' + str(self.request.get('latlng'))
+			+ '?entry.' + str(config_json['feedback_latlng_var']) + '=' + str(self.request.get('latlng'))
 		self.redirect(url)
 
 
