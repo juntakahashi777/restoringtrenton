5918118794563b1f294ff28da7275847d59f21ff
Jun Takahashi <jtakahas@princeton.edu>
2015-04-19 12:55:24 -0400

    formatting

--- js/client_config.js
+++ js/client_config.js
@@ -8,22 +8,22 @@ var config =
     additional_attrib : 'Created by Iana Dikidjieva for <a href="http://www.restoringtrenton.org">Restoring Trenton</a><br>',
    	sql_url : 'http://restoring-trenton.cartodb.com/api/v2/sql?q=',
    	sidebar_content : "<h1>Advanced Search</h1>\
-    <form id='advanced_search' onSubmit='return advanced_geosearch();'>\
-    <fieldset>\
-    <legend>Query:</legend>\
-    Address:<br>\
-    <input type='text' id='address'>\
-    <br>\
-    Lot Type:<br>\
-    <select id='parc_type'>\
-    <option value='any'>Select a lot type</option>\
-    <option value='VACANT BLDG'>Vacant Building</option>\
-    <option value='VACANT LOT'>Vacant Lot</option>\
-    <option value='OCCUPIED BLDG'>Occupied Building</option>\
-    </select>\
-    <br><br>\
-    <input type='submit' value='Go'>\
-    </fieldset>\
-    </form>\
+	    <form id='advanced_search' onSubmit='return advanced_geosearch();'>\
+	    <fieldset>\
+	    <legend>Query:</legend>\
+	    Address:<br>\
+	    <input type='text' id='address'>\
+	    <br>\
+	    Lot Type:<br>\
+	    <select id='parc_type'>\
+	    <option value='any'>Select a lot type</option>\
+	    <option value='VACANT BLDG'>Vacant Building</option>\
+	    <option value='VACANT LOT'>Vacant Lot</option>\
+	    <option value='OCCUPIED BLDG'>Occupied Building</option>\
+	    </select>\
+	    <br><br>\
+	    <input type='submit' value='Go'>\
+	    </fieldset>\
+	    </form>\
     "
 }
