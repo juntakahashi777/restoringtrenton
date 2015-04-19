711082ee35834a1571d38c19dc0d1e7e9715143a
Jun Takahashi <jtakahas@princeton.edu>
2015-04-19 14:02:25 -0400

    added leaflet-navbar package

--- js/map_init.js
+++ js/map_init.js
@@ -96,6 +96,11 @@ function init(){
   map.addControl(sidebar);
   sidebar.setContent(config.sidebar_content);
   // -------------------------
+  // Add navbar module
+
+  L.control.navbar().addTo(map);
+
+  // -------------------------
 
 
   //holding the different layers we're creating from the cartodb base ('trenton_parcels', loaded from our cartoUrl)
